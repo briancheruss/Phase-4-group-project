6c72948e56e83ead5a41fd73e3c8c477e53526b7
Arnold Juma <+>
2024-01-23 02:18:26 +0300

    creation of migrations and generation of the table

---
+++ server/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, make_response, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+from models import db, User, Review, Property
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return '<h1>REAL ESTATE MANAGEMENT SYSTEM</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
