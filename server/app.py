cf6fef2f2bd017f0201dbd87d9c2e3f01f2ba8fd
Arnold-N-Juma <144280149+Arnold-N-Juma@users.noreply.github.com>
2024-01-23 13:18:01 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -2,8 +2,13 @@ from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+from views import *
+
 
 from models import db, User, Review, Property
+
 
 app = Flask(__name__)
 CORS(app)
@@ -16,9 +21,27 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return '<h1>REAL ESTATE MANAGEMENT SYSTEM</h1>'
+jwt = JWTManager()
+app.config["JWT_SECRET_KEY"] = "fjhjdjhfiskyfvdgvydklvsrfl"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
+jwt.init_app(app)
+
+
+app.register_blueprint(user_bp)
+app.register_blueprint(review_bp)
+app.register_blueprint(property_bp)
+app.register_blueprint(auth_bp)
+
+# JWT LOADER
+@jwt.token_in_blocklist_loader
+def token_in_blocklist_callback(jwt_header, jwt_data):
+    jti = jwt_data['jti']
+    token = TokenBlocklist.query.filter_by(jti=jti).first()
+    if token:
+        return token 
+    else:
+        return None
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
